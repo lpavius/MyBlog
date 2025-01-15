0290c9dab9edd01b00e315e159ff55d390e34863
lpavius <lpavius@student.42.fr>
2025-01-15 15:06:23 +0100

    CRUD Category

--- src/main/java/org/wildcodeschool/myblog/controller/CategoryController.java
+++ src/main/java/org/wildcodeschool/myblog/controller/CategoryController.java
@@ -2,12 +2,11 @@ package org.wildcodeschool.myblog.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.wildcodeschool.myblog.model.Category;
 import org.wildcodeschool.myblog.repository.CategoryRepository;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/categories")
@@ -23,5 +22,40 @@ public class CategoryController {
     private ResponseEntity<Category> createCategory(@RequestBody Category category) {
         Category saveCategory = categoryRepository.save(category);
         return ResponseEntity.status(HttpStatus.CREATED).body(saveCategory);
+    }
+
+    @GetMapping
+    private ResponseEntity<List<Category>> getAllCategories() {
+        List<Category> categories = categoryRepository.findAll();
+        if (categories.isEmpty())
+            return ResponseEntity.noContent().build();
+        return ResponseEntity.ok(categories);
+    }
+
+    @GetMapping("/{id}")
+    private ResponseEntity<Category> getArticleById(@PathVariable Long id) {
+        Category category = categoryRepository.findById(id).orElse(null);
+        if (category == null)
+            return ResponseEntity.notFound().build();
+        return ResponseEntity.ok(category);
+    }
+
+    @PutMapping("/{id}")
+    private ResponseEntity<Category> updateCategory(@PathVariable Long id, @RequestBody Category categoryDetails) {
+        Category category = categoryRepository.findById(id).orElse(null);
+        if (category == null)
+            return ResponseEntity.notFound().build();
+        category.setName(categoryDetails.getName());
+        Category updateCategory = categoryRepository.save(category);
+        return ResponseEntity.ok(updateCategory);
+    }
+
+    @DeleteMapping("/{id}")
+    private ResponseEntity<Category> deleteCategory(@PathVariable Long id) {
+        Category category = categoryRepository.findById(id).orElse(null);
+        if (category == null)
+            return ResponseEntity.notFound().build();
+        categoryRepository.delete(category);
+        return ResponseEntity.noContent().build();
     }
 }
