00c769d02eb6c36e8e950881ed796fe9bbf2dc0b
lpavius <lpavius@student.42.fr>
2025-02-11 23:54:44 +0100

    Les Services - Image

---
+++ src/main/java/org/wildcodeschool/myblog/service/ImageService.java
@@ -0,0 +1,65 @@
+package org.wildcodeschool.myblog.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.*;
+import org.wildcodeschool.myblog.dto.ImageDTO;
+import org.wildcodeschool.myblog.mapper.ImageMapper;
+import org.wildcodeschool.myblog.model.Image;
+import org.wildcodeschool.myblog.repository.ImageRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class ImageService {
+
+    private final ImageRepository imageRepository;
+    private final ImageMapper imageMapper;
+
+    public ImageService(ImageRepository imageRepository, ImageMapper imageMapper) {
+        this.imageRepository = imageRepository;
+        this.imageMapper = imageMapper;
+    }
+
+    public List<ImageDTO> getAllImages() {
+        List<Image> images = imageRepository.findAll();
+        if (images.isEmpty()) {
+            return null;
+        }
+        return images.stream()
+                .map(imageMapper::convertToDTO)
+                .collect(Collectors.toList());
+    }
+
+    public ImageDTO getImageById(@PathVariable Long id) {
+        Image image = imageRepository.findById(id).orElse(null);
+        if (image == null) {
+            return null;
+        }
+        return imageMapper.convertToDTO(image);
+    }
+
+    public ImageDTO createImage(@RequestBody Image image) {
+        Image savedImage = imageRepository.save(image);
+        return imageMapper.convertToDTO(savedImage);
+    }
+
+    public ImageDTO updateImage(@PathVariable Long id, @RequestBody Image imageDetails) {
+        Image image = imageRepository.findById(id).orElse(null);
+        if (image == null) {
+            return null;
+        }
+        image.setUrl(imageDetails.getUrl());
+        Image updatedImage = imageRepository.save(image);
+        return imageMapper.convertToDTO(updatedImage);
+    }
+
+    public boolean deleteImage(@PathVariable Long id) {
+        Image image = imageRepository.findById(id).orElse(null);
+        if (image == null) {
+            return false;
+        }
+        imageRepository.delete(image);
+        return true;
+    }
+}
